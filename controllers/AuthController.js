ee3827acc330afd9ca3d2f75ee5283b24f1b18b5
gabrielodd <gabrieloddone@id.uff.br>
2022-10-08 04:06:01 -0300

    Registro e logout

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -20,5 +20,37 @@ module.exports = class AuthController {
 
       return
     }
+
+    const checkIfUserExists = await User.findOne({where: {email: email}})
+
+    if(checkIfUserExists) {
+      req.flash('message', 'O e-mail já está em uso!')
+      res.render('auth/register')
+
+      return
+    }
+
+    const salt = bcrypt.genSaltSync(10)
+    const hashedPassword = bcrypt.hashSync(password, salt)
+
+    const user = {
+      name, email, password: hashedPassword
+    }
+
+    try{
+      const createdUser = await User.create(user)
+
+      req.session.userid = createdUser.id
+      req.flash('message', 'Cadastro realizado com sucesso!')
+
+      res.redirect('/')
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  static logout(req, res) {
+    req.session.destroy()
+    res.redirect('/login')
   }
 }
